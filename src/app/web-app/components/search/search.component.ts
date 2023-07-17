2c7165c59b4e62b859e369dfac1b0d206a0498a1
Juan Tasayco <juantasayco266@gmail.com>
2023-07-17 12:01:47 -0500

    modifiedPlaceholderColor

--- src/app/web-app/components/search/search.component.ts
+++ src/app/web-app/components/search/search.component.ts
@@ -1,38 +1,40 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { debounceTime, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  @ViewChild('movie') movie!: ElementRef;
+  debouncer: Subject<string> = new Subject();
+  @Output() sendMovie: EventEmitter<string> = new EventEmitter();
 
-  @ViewChild("movie") movie!: ElementRef;
-  debouncer: Subject<string> = new Subject;
-  @Output() sendMovie: EventEmitter<string> = new EventEmitter;
-
-  constructor() { }
+  constructor() {}
   ngOnInit(): void {
-
-    this.debouncer
-      .pipe(
-        debounceTime(300)
-      )
-      .subscribe(result => {
-        let textCap!: any;
-        result = result.trimStart().trimEnd();
-        if (result) {
-          textCap = result.split(" ");
-          textCap = textCap.map((texto: string) => texto[0].toUpperCase() + texto.slice(1))
-          this.sendMovie.emit(textCap);
-        }
-        this.sendMovie.emit(result);
-      })
+    this.debouncer.pipe(debounceTime(300)).subscribe((result) => {
+      let textCap!: any;
+      result = result.trimStart().trimEnd();
+      if (result) {
+        textCap = result.split(' ');
+        textCap = textCap.map(
+          (texto: string) => texto[0].toUpperCase() + texto.slice(1)
+        );
+        this.sendMovie.emit(textCap);
+      }
+      this.sendMovie.emit(result);
+    });
   }
 
   obtenerMovie(event: any) {
     this.debouncer.next(event.target.value);
   }
-
 }
